311193e234c81d7b43072eaa624e8a183629cb24
Meaghan Bass <meaghan.bass@gmail.com>
2022-01-28 21:46:06 -0600

    init

--- project/src/App.js
+++ project/src/App.js
@@ -1,6 +1,17 @@
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { getLCP, getFID, getCLS } from "web-vitals";
 import { GridWrap, GridRow, GridColumn } from "emotion-flex-grid";
+
+if (typeof window !== "undefined") {
+  getCLS(console.log);
+  getFID(console.log);
+  getLCP(console.log);
+
+  const Axe = require("@axe-core/react");
+  Axe(React, 1000);
+}
 
 function App() {
   return (

1618a9909a792dfe3fe31dc2e337d76560c1a829
Ali8150 <61247523+Ali8150@users.noreply.github.com>
2022-02-07 19:26:17 -0500

    Mohammad Ali Baal & spots

--- project/src/App.js
+++ project/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect,useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { getLCP, getFID, getCLS } from "web-vitals";
@@ -13,18 +13,88 @@ if (typeof window !== "undefined") {
   Axe(React, 1000);
 }
 
-function App() {
+// feature component
+function FeatureComponent(props) {
+  let { title,description,buttonText } = props;
   return (
+    <div className='featureComponment'>
+      <div className="featureComponmentTitle">{title}</div>
+      <div className="featureComponmentDescription">{description} </div>
+      <div className="featureComponmentButtonText"> <a href=""> {buttonText} </a> </div>
+    </div>
+  );
+};
+
+// each row
+function FeatureRow(props) {
+  var rowData = props.rowList;
+  var temp = [];
+
+  rowData.forEach((element,index) => {
+    let {title,description} = element;
+
+    temp.push(
+      <GridColumn key={"column_"+index} width={4}>
+         <FeatureComponent key={index} title={title} description={description} buttonText="Learn more"/>  
+      </GridColumn>  
+    )
+  });
+
+  return temp;
+}
+
+function App() {
+  const [featureList,setFeatureList] = useState([]);
+
+  function updatedList(numRow,result) {
+    var newList = [];
+    while (result.length > 0) {
+      newList.push(result.splice(0, numRow));
+    }
+
+    return newList;
+  };
+
+  // when the App loads
+  useEffect(() => {
+    fetch('https://mocki.io/v1/b9c63035-97c5-40a0-b45c-2abdf5261bdf')
+      .then(res => res.json()) // parsing the data to json
+      .then(
+        (result) => {
+             if(Array.isArray(result) && result.length !== 0) {
+              // first 6 element
+              var result = result.slice(0,6);
+              var updatedResult = updatedList(3,result);
+              var rowTemplate = [];
+
+              updatedResult.forEach(
+                (row,index) => {
+                    rowTemplate.push(
+                      <GridRow className="featureRow" key={"row_"+index}>
+                        <FeatureRow
+                          rowList={row}
+                        />
+                      </GridRow>  
+                    );
+                }
+              );
+              setFeatureList(rowTemplate);   
+        }
+      }
+        ,
+        () => {
+          alert("cannot connect to service");
+        }
+      );
+  });
+
+  // main template
+  return (    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <div className ='feature'>
+        <p>Experience The Cardo </p>
+        { featureList }
+      </div>
     </div>
   );
 }
